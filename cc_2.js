3f3e1c6c99b856c58d658b3dd9ff4ed4d30ec93b
avery.white <averydouglascwhite@usf.edu>
2025-01-25 12:12:33 -0500

    Task 1 - Store Inventory

---
+++ cc_2.js
@@ -0,0 +1,8 @@
+// Task 1 - Store Inventory
+let products = ["laptop", "mouse", "keyboard", "monitor", "printer"]; // Array of products
+products.push("headset"); // Adding Headset to the list
+products.pop(); // Removes last elemnet from the list - headset
+console.log("Updated Product List:", products);
+// Task 1 - Store Inventory
+
+
